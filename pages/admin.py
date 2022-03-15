1f5e3bd37423019ccf3d4c265358240450c8c6b6
Harsh <harsh.tiwari.1242001@gmail.com>
2022-03-16 01:47:10 +0530

    changes updated

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Team
 
 # Register your models here.
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display=('id','first_name','designation','created_date')
+
+admin.site.register(Team,TeamAdmin)
